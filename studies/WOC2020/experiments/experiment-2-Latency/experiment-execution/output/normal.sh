3a5fd47a44a865ec14e0fe4d2c0351c5adbda626
Bo Kleynen <kleynenbo@gmail.com>
2021-06-07 00:55:54 +0200

    Add results

--- studies/WOC2020/experiments/experiment-2-Latency/experiment-execution/output/normal.sh
+++ studies/WOC2020/experiments/experiment-2-Latency/experiment-execution/output/normal.sh
@@ -1,1 +1,1 @@
-java -jar /exp/lib/scalar-1.0.0.jar /exp/etc/platform.properties /exp/etc/normal-incremental-experiment.properties
+java -jar /exp/lib/scalar-1.0.0.jar /exp/etc/platform.properties /exp/etc/normal-group-experiment.properties
